37ed9def770618ec200ef5970cacb4fe301ee9f7
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:03:06 +0300

    Add GetAllPrayersUseCase for retrieving all prayer entities

---
+++ lib/features/prayers/domain/usecases/get_all_prayers_usecase.dart
@@ -0,0 +1,12 @@
+import '../entities/prayer_entity.dart';
+import '../../../../data/repositories/prayer_repository.dart';
+
+class GetAllPrayersUseCase {
+  const GetAllPrayersUseCase(this._repository);
+
+  final PrayerRepository _repository;
+
+  Future<List<PrayerEntity>> call() {
+    return _repository.getPrayers();
+  }
+}
