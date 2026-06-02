334111993d320ab1f3416c9e820a1404af1f8086
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:01:06 +0300

    Add AssetPaths class for centralized asset path management

---
+++ lib/core/constants/asset_paths.dart
@@ -0,0 +1,18 @@
+abstract final class AssetPaths {
+  static const images = 'assets/images/';
+  static const logo = 'assets/images/logo/';
+  static const icons = 'assets/images/icons/';
+  static const illustrations = 'assets/images/illustrations/';
+
+  static const content = 'assets/content/';
+  static const categories = 'assets/content/categories/';
+  static const prayersEn = 'assets/content/prayers/en/';
+  static const prayersSw = 'assets/content/prayers/sw/';
+  static const novenasEn = 'assets/content/novenas/en/';
+  static const novenasSw = 'assets/content/novenas/sw/';
+  static const rosaryEn = 'assets/content/rosary/en/';
+  static const rosarySw = 'assets/content/rosary/sw/';
+  static const metadata = 'assets/content/metadata/';
+
+  static const legacyPrayers = 'assets/data/prayers.json';
+}
