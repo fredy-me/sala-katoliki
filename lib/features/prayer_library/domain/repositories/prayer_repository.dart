70f48d676e43136ebd8f41546937f06b32992cc9
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:00:09 +0300

    Remove PrayerRepository to simplify prayer library structure

--- lib/features/prayer_library/domain/repositories/prayer_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../entities/prayer_entity.dart';
-
-abstract interface class PrayerRepository {
-  Future<List<PrayerEntity>> getPrayers();
-
-  Future<PrayerEntity?> getPrayerById(String id);
-}
