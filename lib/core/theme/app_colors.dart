987f6a65fefabc5b00b7c8337de06988c83ac022
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 18:58:27 +0300

    Add app_colors.dart file to define color constants for theming

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const night = Color(0xFF0B111C);
+  static const nightPanel = Color(0xFF121A29);
+  static const nightPanelAlt = Color(0xFF1B2536);
+  static const border = Color(0xFF293447);
+  static const primary = Color(0xFFA9C8F5);
+  static const primaryStrong = Color(0xFF8FB4E8);
+  static const text = Color(0xFFF9F4EC);
+  static const mutedText = Color(0xFF9FB4D4);
+  static const dimText = Color(0xFF73829A);
+  static const warm = Color(0xFFD4A574);
+  static const sage = Color(0xFF6B8E23);
+}
