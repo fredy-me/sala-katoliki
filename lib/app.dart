cb51568cd47897794ba9f72b0f4211a18baffb67
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:00:47 +0300

    Fix import paths for AppTheme and AppRouter in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'config/routes/app_router.dart';
-import 'config/theme/app_theme.dart';
+import 'core/theme/app_theme.dart';
+import 'routes/app_router.dart';
 
 class SalaKatolikiApp extends ConsumerWidget {
   const SalaKatolikiApp({super.key});
