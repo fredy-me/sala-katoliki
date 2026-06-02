76e68c9c5384fdc1ebc8f77923310a055256e1be
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:22:06 +0300

    Refactor AppShell to use AppBottomNav for navigation and simplify destination handling

--- lib/shared/widgets/app_shell.dart
+++ lib/shared/widgets/app_shell.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+
+import 'app_bottom_nav.dart';
 
 class AppShell extends StatelessWidget {
   const AppShell({required this.location, required this.child, super.key});
@@ -8,41 +9,24 @@ class AppShell extends StatelessWidget {
   final Widget child;
 
   static const _destinations = [
-    _ShellDestination('/today', 'Today', Icons.home_outlined),
-    _ShellDestination('/prayers', 'Pray', Icons.menu_book_outlined),
-    _ShellDestination('/novenas', 'Novenas', Icons.calendar_month_outlined),
-    _ShellDestination('/library', 'Library', Icons.auto_stories_outlined),
+    AppBottomNavDestination('/today', 'Today', Icons.home_outlined),
+    AppBottomNavDestination('/prayers', 'Pray', Icons.menu_book_outlined),
+    AppBottomNavDestination(
+      '/novenas',
+      'Novenas',
+      Icons.calendar_month_outlined,
+    ),
+    AppBottomNavDestination('/library', 'Library', Icons.auto_stories_outlined),
   ];
 
   @override
   Widget build(BuildContext context) {
-    final selectedIndex = _destinations.indexWhere(
-      (destination) => location == destination.path,
-    );
-
     return Scaffold(
       body: SafeArea(bottom: false, child: child),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: selectedIndex < 0 ? 0 : selectedIndex,
-        onDestinationSelected: (index) {
-          context.go(_destinations[index].path);
-        },
-        destinations: [
-          for (final destination in _destinations)
-            NavigationDestination(
-              icon: Icon(destination.icon),
-              label: destination.label,
-            ),
-        ],
+      bottomNavigationBar: AppBottomNav(
+        location: location,
+        destinations: _destinations,
       ),
     );
   }
-}
-
-class _ShellDestination {
-  const _ShellDestination(this.path, this.label, this.icon);
-
-  final String path;
-  final String label;
-  final IconData icon;
 }
