d84a1ea98b01f5f8842f6afa23ae9b55a852b4f8
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:01:35 +0300

    Add PrayerModel class for mapping prayer data from JSON

---
+++ lib/data/models/prayer_model.dart
@@ -0,0 +1,23 @@
+import '../../features/prayers/domain/entities/prayer_entity.dart';
+
+class PrayerModel extends PrayerEntity {
+  const PrayerModel({
+    required super.id,
+    required super.category,
+    required super.categorySw,
+    required super.titles,
+    required super.texts,
+    super.isFavorite,
+  });
+
+  factory PrayerModel.fromJson(Map<String, dynamic> json) {
+    return PrayerModel(
+      id: json['id'] as String,
+      category: json['category'] as String,
+      categorySw: json['categorySw'] as String,
+      titles: Map<String, String>.from(json['titles'] as Map),
+      texts: Map<String, String>.from(json['texts'] as Map),
+      isFavorite: json['isFavorite'] as bool? ?? false,
+    );
+  }
+}
