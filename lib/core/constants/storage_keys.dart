6cc5abd83267c59255d659488a1e62728a5a055c
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:58:11 +0300

    Refactor StorageKeys: Add recentPrayerIds constant for improved data management

--- lib/core/constants/storage_keys.dart
+++ lib/core/constants/storage_keys.dart
@@ -3,6 +3,7 @@ abstract final class StorageKeys {
   static const themeMode = 'theme_mode';
   static const fontSize = 'font_size';
   static const favoritePrayerIds = 'favorite_prayer_ids';
+  static const recentPrayerIds = 'recent_prayer_ids';
   static const reminderEnabled = 'reminder_enabled';
   static const reminderTime = 'reminder_time';
   static const rosaryMysteryId = 'rosary_mystery_id';
