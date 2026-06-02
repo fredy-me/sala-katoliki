ad97e55cdda1abd02cbb9fb7d079d636450ea4a5
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:00:57 +0300

    Add AppConstants class for application-wide constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,6 @@
+abstract final class AppConstants {
+  static const appName = 'Sala Katoliki';
+  static const appVersion = '0.1.0';
+  static const defaultLanguageCode = 'en';
+  static const supportedLanguageCodes = ['en', 'sw'];
+}
