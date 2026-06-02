9370eee41c8614dd3d9c7ce77feb530cc729f1ad
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:42:17 +0300

    Refactor PrayerRepository: Simplify getPrayerById method signature

--- lib/data/repositories/prayer_repository.dart
+++ lib/data/repositories/prayer_repository.dart
@@ -3,8 +3,5 @@ import '../../features/prayers/domain/entities/prayer_entity.dart';
 abstract interface class PrayerRepository {
   Future<List<PrayerEntity>> getPrayers({String languageCode = 'sw'});
 
-  Future<PrayerEntity?> getPrayerById(
-    String id, {
-    String languageCode = 'sw',
-  });
+  Future<PrayerEntity?> getPrayerById(String id, {String languageCode = 'sw'});
 }
