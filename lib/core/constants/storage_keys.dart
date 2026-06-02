b49cd80c3304c81e7f0232de4ac626f62a1e83ff
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:01:14 +0300

    Add StorageKeys class for centralized storage key management

---
+++ lib/core/constants/storage_keys.dart
@@ -0,0 +1,12 @@
+abstract final class StorageKeys {
+  static const selectedLanguage = 'selected_language';
+  static const themeMode = 'theme_mode';
+  static const fontSize = 'font_size';
+  static const favoritePrayerIds = 'favorite_prayer_ids';
+  static const reminderEnabled = 'reminder_enabled';
+  static const reminderTime = 'reminder_time';
+  static const rosaryMysteryId = 'rosary_mystery_id';
+  static const rosaryStepIndex = 'rosary_step_index';
+  static const activeNovenaId = 'active_novena_id';
+  static const completedNovenaDays = 'completed_novena_days';
+}
