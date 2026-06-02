c6246af595e839eefbdc65c265d2853bce925a7d
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:40:21 +0300

    Update PrayerRepository to include languageCode parameter for fetching prayers

--- lib/data/repositories/prayer_repository.dart
+++ lib/data/repositories/prayer_repository.dart
@@ -1,7 +1,10 @@
 import '../../features/prayers/domain/entities/prayer_entity.dart';
 
 abstract interface class PrayerRepository {
-  Future<List<PrayerEntity>> getPrayers();
+  Future<List<PrayerEntity>> getPrayers({String languageCode = 'sw'});
 
-  Future<PrayerEntity?> getPrayerById(String id);
+  Future<PrayerEntity?> getPrayerById(
+    String id, {
+    String languageCode = 'sw',
+  });
 }
