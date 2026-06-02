5c05574d350bc219ec3535432b96ba3baf02b40a
fredy-me <fredymelkizedek@gmail.com>
2026-06-02 19:14:28 +0300

    Fix import path for FeaturePlaceholderScreen and update subtitle in SettingsScreen

--- lib/features/settings/presentation/screens/settings_screen.dart
+++ lib/features/settings/presentation/screens/settings_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../../shared/presentation/widgets/feature_placeholder_screen.dart';
+import '../../../../shared/widgets/feature_placeholder_screen.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -9,7 +9,8 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const FeaturePlaceholderScreen(
       title: 'Mipangilio',
-      subtitle: 'Lugha, mandhari, vikumbusho, na akaunti ya majaribio.',
+      subtitle:
+          'Lugha, mandhari, vikumbusho, ukubwa wa maandishi, na taarifa za programu.',
       icon: Icons.settings_outlined,
       primaryActionLabel: 'Badili lugha',
     );
