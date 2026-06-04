0e148e168b0f25f9101bc50dd1fbe29029524db0
fredy-me <fredymelkizedek@gmail.com>
2026-06-04 10:30:34 +0300

    Update minSdk version in build.gradle.kts to use flutter.minSdkVersion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         applicationId = "tech.kilimanjaro.salakatoliki"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
