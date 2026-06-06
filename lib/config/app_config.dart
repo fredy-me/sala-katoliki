4d3bfa4bee69714c7021aa64fb64d2be6d605a98
fredy-me <fredymelkizedek@gmail.com>
2026-06-06 23:13:41 +0300

    Add factory constructor to AppConfig for environment-based initialization

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -6,9 +6,24 @@ class AppConfig {
     this.appName = AppConstants.appName,
     this.version = AppConstants.appVersion,
     this.environment = Environment.development,
+    this.apiBaseUrl = '',
+    this.sentryDsn = '',
+    this.analyticsEnabled = false,
   });
+
+  factory AppConfig.fromEnvironment() {
+    return AppConfig(
+      environment: Environment.current,
+      apiBaseUrl: const String.fromEnvironment('API_BASE_URL'),
+      sentryDsn: const String.fromEnvironment('SENTRY_DSN'),
+      analyticsEnabled: const bool.fromEnvironment('ANALYTICS_ENABLED'),
+    );
+  }
 
   final String appName;
   final String version;
   final Environment environment;
+  final String apiBaseUrl;
+  final String sentryDsn;
+  final bool analyticsEnabled;
 }
