564e66d96ff85f606d932853d75ed6e1d74f775d
fredy-me <fredymelkizedek@gmail.com>
2026-06-06 23:01:45 +0300

    Refactor AppShell to use ConsumerWidget and localize bottom navigation labels

--- lib/shared/widgets/app_shell.dart
+++ lib/shared/widgets/app_shell.dart
@@ -1,32 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../core/localization/localization_providers.dart';
 import 'app_bottom_nav.dart';
 
-class AppShell extends StatelessWidget {
+class AppShell extends ConsumerWidget {
   const AppShell({required this.location, required this.child, super.key});
 
   final String location;
   final Widget child;
 
-  static const _destinations = [
-    AppBottomNavDestination('/today', 'Today', Icons.home_outlined),
-    AppBottomNavDestination('/prayers', 'Pray', Icons.menu_book_outlined),
-    AppBottomNavDestination(
-      '/novenas',
-      'Novenas',
-      Icons.calendar_month_outlined,
-    ),
-    AppBottomNavDestination('/library', 'Library', Icons.auto_stories_outlined),
-  ];
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final strings = _BottomNavStrings(ref.watch(activeLanguageProvider));
+
     return Scaffold(
       body: SafeArea(bottom: false, child: child),
       bottomNavigationBar: AppBottomNav(
         location: location,
-        destinations: _destinations,
+        destinations: [
+          AppBottomNavDestination('/today', strings.today, Icons.home_outlined),
+          AppBottomNavDestination(
+            '/prayers',
+            strings.pray,
+            Icons.menu_book_outlined,
+          ),
+          AppBottomNavDestination(
+            '/novenas',
+            strings.novenas,
+            Icons.calendar_month_outlined,
+          ),
+          AppBottomNavDestination(
+            '/library',
+            strings.library,
+            Icons.auto_stories_outlined,
+          ),
+        ],
       ),
     );
   }
+}
+
+class _BottomNavStrings {
+  const _BottomNavStrings(this.languageCode);
+
+  final String languageCode;
+
+  bool get _sw => languageCode == 'sw';
+
+  String get today => _sw ? 'Leo' : 'Today';
+  String get pray => _sw ? 'Sala' : 'Pray';
+  String get novenas => _sw ? 'Novenas' : 'Novenas';
+  String get library => _sw ? 'Maktaba' : 'Library';
 }
