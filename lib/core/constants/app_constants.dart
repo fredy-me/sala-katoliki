5b0fdda98a265b4e13369c5ba422bed0a2aeccfd
fredy-me <fredymelkizedek@gmail.com>
2026-06-21 14:05:45 +0300

    Bump app version to 1.0.1

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 abstract final class AppConstants {
   static const appName = 'Sala Katoliki';
-  static const appVersion = '1.0.0';
+  static const appVersion = '1.0.1';
   static const defaultLanguageCode = 'en';
   static const supportedLanguageCodes = ['en', 'sw'];
 }
