3f0d7b8780420ae85790567429d29a0e126bd5cf
fredy-me <fredymelkizedek@gmail.com>
2026-07-22 15:03:54 +0300

    Update target SDK to 36 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
     defaultConfig {
         applicationId = "com.busaradigital.salakatoliki"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
