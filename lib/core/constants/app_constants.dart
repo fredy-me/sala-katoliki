72f47a83f446420bdd2a95338b5f0a98a515289e
fredy-me <fredymelkizedek@gmail.com>
2026-07-22 15:04:31 +0300

    Update app version to 1.0.6 in app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 abstract final class AppConstants {
   static const appName = 'Sala Katoliki';
-  static const appVersion = '1.0.5';
+  static const appVersion = '1.0.6';
   static const defaultLanguageCode = 'en';
   static const supportedLanguageCodes = ['en', 'sw'];
 }
