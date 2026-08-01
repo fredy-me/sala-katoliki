8ca34fa19317acb751c161123bf55f1423b378a5
fredy-me <fredymelkizedek@gmail.com>
2026-08-01 18:52:26 +0300

    feat: enhance main function to handle widget launch and provide initial location

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:home_widget/home_widget.dart';
 
 import 'app.dart';
+import 'routes/app_router.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-  runApp(const ProviderScope(child: SalaKatolikiApp()));
+  final widgetUri = await HomeWidget.initiallyLaunchedFromHomeWidget();
+  runApp(
+    ProviderScope(
+      overrides: [
+        if (widgetUri != null)
+          appInitialLocationProvider.overrideWithValue(widgetUri.toString()),
+      ],
+      child: const SalaKatolikiApp(),
+    ),
+  );
 }
