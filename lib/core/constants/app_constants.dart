c2a69ebbfb43e93e775b860fd1199f1845a9ee09
fredy-me <fredymelkizedek@gmail.com>
2026-09-01 13:42:46 +0300

    chore: update app version to 1.0.10 in app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 abstract final class AppConstants {
   static const appName = 'Sala Katoliki';
-  static const appVersion = '1.0.9';
+  static const appVersion = '1.0.10';
   static const defaultLanguageCode = 'en';
   static const supportedLanguageCodes = ['en', 'sw'];
 }
